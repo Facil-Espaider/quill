affa489ab1c903b0b0cd43aa3b265e1c27616ef8
Filipe de Mello Cunha <filipe.mello@facil.com.br>
2025-05-13 16:38:09 -0300

    Largura natural para imagens.

--- formats/image.ts
+++ formats/image.ts
@@ -1,7 +1,7 @@
 import { EmbedBlot } from 'parchment';
 import { sanitize } from './link';
 
-const ATTRIBUTES = ['alt', 'height', 'width'];
+const ATTRIBUTES = ['alt', 'height', 'width', 'naturalWidth'];
 
 class Image extends EmbedBlot {
   static blotName = 'image';
